99b13a83b29a4c5ddae62c3e9e493b84883302ad
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-01-29 14:21:50 +0700

    Update ptt.php

--- ptt.php
+++ ptt.php
@@ -1,6 +1,3 @@
-<html>
-<body>
-
 <?php
 
 //รับตัวแปรจาก $_POST["name"] มาเก็บไว้ใน $Language
@@ -35,17 +32,39 @@ $objectResult = $client->__soapCall($methodName, array('parameters' => $params),
 //echo $objectResult->CurrentOilPriceResult;
 
  $ob = $objectResult->CurrentOilPriceResult;
-            $xml = new SimpleXMLElement($ob);
+
+		   $xml = new SimpleXMLElement($ob);
            
                // PRICE_DATE , PRODUCT ,PRICE
-              foreach ($xml  as  $key =>$val) {  
+              //foreach ($xml  as  $key =>$val) {  
               
-            if($val->PRICE != ''){
-              echo $val->PRICE_DATE.'  '.$val->PRODUCT .'  ราคา  '.$val->PRICE.' บาท<br>';
-                }
+           //if($val->PRICE != ''){
+             // echo $val->PRICE_DATE.'  '.$val->PRODUCT .'  ราคา  '.$val->PRICE.' บาท<br>';
+                
+			//	}
+//$ptt = json_encode($val);
+//echo $ptt;
 
-               }
+// $data_string = json_encode($val); //ทำให้เป็น json ด้วยฟังก์ชัน json_encode
+ 
+// $ch = curl_init('http://localhost/ptt.php'); //ตรงนี้อย่าลืมเปลี่ยนเป็น url ที่ต้องการส่งค่าไปนะครับ
+ 
+// //ในกรณีที่ต้องการส่งเป็น method อื่น เช่น DELETE, PUT, HEAD ก็เปลี่ยนได้ที่นี่ครับ
+// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+ 
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//     'Content-Type: application/json', //ระบุว่าค่าที่ส่งไปเป็นแบบ json
+//     'Content-Length: ' . strlen($data_string))  //บอกขนาดของ json ที่ส่งไปด้วย
+// );
+ 
+// $result = curl_exec($ch);
+// curl_close($ch);
+
+     // } 
+      
+      echo json_encode($xml);     
 
 ?>
-</body>
-</html>
+
