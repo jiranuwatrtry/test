b0d89f590a071bcd1f1ccaf0aace783c028839d0
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-01-31 13:16:21 +0700

    Update weather.php

--- weather.php
+++ weather.php
@@ -34,28 +34,28 @@ $province = "เชียงใหม่";
                 foreach ($v as $ky => $vy) { 
                    
 
-                    if($ky == 'Province'){echo 'จังหวัด : '.$vy.'\n'; } 
-                    if($ky == 'StationNameTh'){echo 'สถานีตรวจวัด : '.$vy.'\n'; }                         
+                    if($ky == 'Province'){echo "จังหวัด : ".$vy."\n"; } 
+                    if($ky == 'StationNameTh'){echo "สถานีตรวจวัด : ".$vy."\n"; }                         
                     if($ky == 'Observe') {foreach ($vy as $kya => $vya) { 
                         
-                        if($kya == 'Time') {  echo 'วันที่ตรวจวัด  :  '.$vya.'\n'; }
+                        if($kya == 'Time') {  echo "วันที่ตรวจวัด  :  ".$vya."\n"; }
                         if($kya == 'Temperature') {foreach ($vya as $kyab => $vyab) { 
-                            if($kyab == 'Value'){echo 'อุณหภูมอากาศปัจจุบัน : '.$vyab.'  Cํ'.'\n';} }} 
+                            if($kyab == 'Value'){echo "อุณหภูมอากาศปัจจุบัน : ".$vyab."  Cํ"."\n";} }} 
                         
                         if($kya == 'MaxTemperature') {foreach ($vya as $kyab => $vyab) { 
-                            if($kyab == 'Value'){echo 'อุณหภูมสุงสุดใน24ชม.ที่ผ่านมา : '.$vyab.'  Cํ'.'\n';} }} 
+                            if($kyab == 'Value'){echo "อุณหภูมสุงสุดใน24ชม.ที่ผ่านมา : ".$vyab."  Cํ"."\n";} }} 
             
                         if($kya == 'MinTemperature') {foreach ($vya as $kyab => $vyab) { 
-                            if($kyab == 'Value'){echo 'อุณหภูมต่ำสุดใน24ชม.ที่ผ่านมา  : '.$vyab.'  Cํ'.'\n';} }}
+                            if($kyab == 'Value'){echo "อุณหภูมต่ำสุดใน24ชม.ที่ผ่านมา  : ".$vyab."  Cํ"."\n";} }}
                         
                         if($kya == 'RelativeHumidity') {foreach ($vya as $kyab => $vyab) { 
-                            if($kyab == 'Value'){echo 'ค่าเฉลี่ยความชื้นสัมพัทธ์ : '.$vyab.'  %'.'\n';} }} 
+                            if($kyab == 'Value'){echo "ค่าเฉลี่ยความชื้นสัมพัทธ์ : ".$vyab."  %"."\n";} }} 
                            
                         if($kya == 'WindSpeed') {foreach ($vya as $kyab => $vyab) { 
-                            if($kyab == 'Value'){echo 'ค่าเฉลี่ยความเร็วลม : '.$vyab.'  km/h'.'\n';} }}    
+                            if($kyab == 'Value'){echo "ค่าเฉลี่ยความเร็วลม : ".$vyab."  km/h"."\n";} }}    
                         
                         if($kya == 'Rainfall') {foreach ($vya as $kyab => $vyab) { 
-                            if($kyab == 'Value'){echo 'ค่าเฉลี่ยปริมาณน้ำฝน : '.$vyab.'  %'.'\n'.'\n';} }}
+                            if($kyab == 'Value'){echo "ค่าเฉลี่ยปริมาณน้ำฝน : ".$vyab."  %"."\n"."\n";} }}
                         }}            
                 } 
             }    
