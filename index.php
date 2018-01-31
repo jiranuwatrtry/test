ea42291a742a1510254ea767bdeceba93d0b6f80
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-01-31 12:39:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -49,6 +49,7 @@ if (!is_null($events['events'])) {
 
 			
 			}else if (strpos($text, 'อากาศ') !== false) {
+				if (strpos($text, 'อากาศ') !== false) {
       $x_tra = str_replace("อากาศ","", $text);
       $pieces = explode(" ", $x_tra);
       $_question = str_replace("|","",$pieces[0]);
@@ -56,7 +57,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $_question." : รับทราบครับ"
     
-    }else{
+    }}else{
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
