2807a541c289273f4f8826d058f2c27295e2fe0a
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-02-01 16:13:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if (!is_null($events['events'])) {
 			}else (strpos($text, 'โทรศัพท์') !== false){
 				$x_tra = str_replace("โทรศัพท์","", $text);
       				$pieces = explode(" ", $x_tra);
-      				$_question = str_replace(" ","",$pieces[0]);
+      				$_question = str_replace("/","",$pieces[0]);
       		
 								
 							$messages = [
