f79bc5c33dce752a98927f2ece2c1f2e892649e6
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-02-05 15:19:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -73,13 +73,12 @@ if (!is_null($events['events'])) {
 			}else if(strpos($text, 'ผลบอล') !== false){
 				$x_tra = str_replace("ผลบอล","", $text);
       				$pieces = explode("|", $x_tra);
-    				
-					 
-								$h= rand(1,2);
-				$p1=str_replace("","",$pieces[$h])
+    				$a=array(str_replace("[","",$pieces[0],str_replace("]","",$pieces[1]);
+					 $random_keys=array_rand($a,2);
+				
 							$messages = [
 								'type' => 'text',
-								'text' => "ผมคิดว่าทีม ".$p1." ชนะครับ"
+								'text' => "ผมคิดว่าทีม ".$a[$random_keys[0]]." ชนะครับ"
 								];
 					
 			
