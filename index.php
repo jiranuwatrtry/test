a12b3c0844e5f33cfa281cfd342603823b27c62a
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-02-27 11:52:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -144,7 +144,7 @@ if (!is_null($events['events'])) {
 				'text' => $result." : รับทราบครับ"
 				
 			];
-				}
+				
 			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
