d580610ad820d94321af384488627bb6c0b12b6d
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-03-02 08:55:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ if (!is_null($events['events'])) {
     				$output = str_replace("account_num","รหัสลูกค้า ", $output);
     				$output = str_replace("response"," ", $output);
     				$output = str_replace(":","", $output);
-    				$output = str_replace(",","", $output);
+    				$output = str_replace(",","\n", $output);
     				$output = str_replace('"','', $output);
 
 
@@ -102,7 +102,7 @@ if (!is_null($events['events'])) {
     				$output = str_replace("service_no","เลขหมายบริการ", $output);
     				$output = str_replace("response","", $output);
     				$output = str_replace(":","", $output);
-    				$output = str_replace(",","", $output);
+    				$output = str_replace(",","\n", $output);
     				$output = str_replace('"','', $output);
 							$messages = [
 								'type' => 'text',
