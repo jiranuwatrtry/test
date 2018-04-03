ec68e61d30848bd0c56defc30253469f92f36f0b
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-04-03 16:37:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -211,13 +211,7 @@ if (!is_null($events['events'])) {
 						$results = curl_exec($chs);
 						curl_close($chs);
 						
-				$messages = [
 				
-				'type' => 'text',
-				'text' => $text
-				
-				
-			];
 			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
