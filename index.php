946d9c688b763d57f7fdc6250c54564c4ac3d10e
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-05-31 13:36:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -112,7 +112,8 @@ if (!is_null($events['events'])) {
 					
 
 			
-			}else if(strpos($text, 'แม็ก') !== false || strpos($text, 'พง') !== false || strpos($text, 'สน') !== false || strpos($text, 'ตี้') !== false){
+			}else if(strpos($text, 'พี่กบ') !== false || strpos($text, 'พี่เต้') !== false || strpos($text, 'ด๊าด') !== false || strpos($text, 'ตี้') !== false 
+				 || strpos($text, 'หมู') !== false || strpos($text, 'แกลว') !== false){
 				
       					$a=array("เป็นความจริงเลยเดียว","โกหกทั้งเพ","มันไม่ใช่ความจริง","แล้วไงล่ะ","หรอ");
 					$random_keys=array_rand($a,2);
